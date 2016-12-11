dc8b4977af66a4bfdf8401dbab8ea346cc5382df
Corey Woodworth <coreywoodworth@Coreys-MacBook-Pro.local>
2016-12-10 21:30:31 -0800

    whole comit

---
+++ opencart/upload/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://104.42.105.27/shopping/opencart/upload/admin/');
+define('HTTP_CATALOG', 'http://104.42.105.27/shopping/opencart/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://104.42.105.27/shopping/opencart/upload/admin/');
+define('HTTPS_CATALOG', 'http://104.42.105.27/shopping/opencart/upload/');
+
+// DIR
+define('DIR_APPLICATION', '/website/potluck/shopping/opencart/upload/admin/');
+define('DIR_SYSTEM', '/website/potluck/shopping/opencart/upload/system/');
+define('DIR_IMAGE', '/website/potluck/shopping/opencart/upload/image/');
+define('DIR_LANGUAGE', '/website/potluck/shopping/opencart/upload/admin/language/');
+define('DIR_TEMPLATE', '/website/potluck/shopping/opencart/upload/admin/view/template/');
+define('DIR_CONFIG', '/website/potluck/shopping/opencart/upload/system/config/');
+define('DIR_CACHE', '/website/potluck/shopping/opencart/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/website/potluck/shopping/opencart/upload/system/storage/download/');
+define('DIR_LOGS', '/website/potluck/shopping/opencart/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/website/potluck/shopping/opencart/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/website/potluck/shopping/opencart/upload/system/storage/upload/');
+define('DIR_CATALOG', '/website/potluck/shopping/opencart/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencartuser');
+define('DB_PASSWORD', 'Kingspade20$!');
+define('DB_DATABASE', 'opencartdb');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ opencart/upload/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://104.42.105.27/shopping/opencart/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://104.42.105.27/shopping/opencart/upload/');
+
+// DIR
+define('DIR_APPLICATION', '/website/potluck/shopping/opencart/upload/catalog/');
+define('DIR_SYSTEM', '/website/potluck/shopping/opencart/upload/system/');
+define('DIR_IMAGE', '/website/potluck/shopping/opencart/upload/image/');
+define('DIR_LANGUAGE', '/website/potluck/shopping/opencart/upload/catalog/language/');
+define('DIR_TEMPLATE', '/website/potluck/shopping/opencart/upload/catalog/view/theme/');
+define('DIR_CONFIG', '/website/potluck/shopping/opencart/upload/system/config/');
+define('DIR_CACHE', '/website/potluck/shopping/opencart/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/website/potluck/shopping/opencart/upload/system/storage/download/');
+define('DIR_LOGS', '/website/potluck/shopping/opencart/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/website/potluck/shopping/opencart/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/website/potluck/shopping/opencart/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencartuser');
+define('DB_PASSWORD', 'Kingspade20$!');
+define('DB_DATABASE', 'opencartdb');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');

a70275de2c6b1c7e202ccab715c2fb6285ad7a93
Corey Woodworth <coreywoodworth@Coreys-MacBook-Pro.local>
2016-12-11 19:11:03 -0800

    December Push

---
+++ music-player-1.0.1/demo/js/myplaylist.js
@@ -0,0 +1,42 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'mix/bho.mp3',
+        oga:'mix/1.ogg',
+        title:'Blow Ya Head Off',
+        artist:'Potluck',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/spa.png'
+	
+    },
+	
+	
+	{
+	    mp3:'mix/better.mp3',
+        oga:'mix/1.ogg',
+        title:'Better Me feat/James Boy',
+        artist:'Potluck',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/spa.png'	
+	}
+	
+	
+	    
+	
+	
+
+];
